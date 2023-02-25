c071b05a763dab6bc0acb11935673e5faccec4e1
Julian <julianser@hotmail.com.ar>
2023-02-25 01:06:18 -0300

    Se realizaron los primeros 5 ejercicios del TP1 de JavaScript

---
+++ js/ejercicio5.js
@@ -0,0 +1,8 @@
+/*
+5.- Escribe un programa de tres líneas que pida un número, pida otro número 
+y escriba el resultado de sumar estos dos números. 
+*/
+
+let num1 = prompt("Escriba un numero");
+let num2 = prompt("Escriba otro numero");
+document.write("La Suma de estos numeros es:" + " " + (parseInt(num1) + parseInt(num2)));
