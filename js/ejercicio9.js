a138c0f3648ee21151e6fb1ca8eddab5e4b1a837
Julian <julianser@hotmail.com.ar>
2023-02-27 16:04:19 -0300

    Se realizo el ejercicio 9

---
+++ js/ejercicio9.js
@@ -0,0 +1,12 @@
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+let frase = prompt("Escriba una frase");
+let i = 0;
+document.write(`La frase (${frase}) tiene las siguientes vocales:`)
+for(i=0;i<=frase.length;i++)
+{
+    if(frase.charAt(i) === "a" || frase.charAt(i) === "e" || frase.charAt(i) === "i" || frase.charAt(i) === "o" || frase.charAt(i) === "u")
+    {
+        document.write(`<br>${frase.charAt (i)}`)
+    }
+}
